e0d09e32d673c8fa732817ff9a55fb8ac1740a9d
test-app-dengage <tayfun.senturk@dengage.com>
2026-01-22 13:58:11 +0300

    Refactor service worker URL handling

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -1,7 +1,1 @@
-
-var swUrl = new URL(location);
-var accountId = swUrl.searchParams.get('account_id') || '';
-var appGuid = swUrl.searchParams.get('app_guid') || '';
-if (accountId && appGuid) {
-    importScripts("https://tst.lib.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
-}
+var a=new URL(location),b="searchParams",c="get",d=a[b][c]("account_id"),e=a[b][c]("app_guid"),f=a[b][c]("version"),g=a[b][c]("hash");if(d&&e){importScripts("https://test-pub.dengage.com/p/push/"+d+"/"+e+(f?("/sdk/"+f):"")+"/dengage_sw"+(g?("."+g):"")+".js")}
